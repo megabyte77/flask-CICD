630c0b994c3118494d6e5bc1b073c929a61aaea1
Shantanu <jackreacherrt@gmail.com>
2025-02-16 19:02:37 +0530

    app file change

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
-
+from flask import Flask
+ 
 app = Flask(__name__)
-
-@app.route('/devops', methods=['GET'])
-def devops_info():
-    return jsonify({
-        "message": "Welcome to the DevOps API!",
-        "tools": ["Docker", "Kubernetes", "Terraform", "Jenkins", "Ansible"],
-        "status": "running"
-    })
-
+ 
+@app.route('/')
+def home():
+    return "Hello, Flask is running inside a container!"
+ 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
